b2fa4abef3cad8049ae07ce18b92ed5237e37318
William Durand <will+git@drnd.me>
2021-04-22 18:11:44 +0200

    Add support for auth0 (#2)

--- addons-wp-headless.php
+++ addons-wp-headless.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Add-ons WP Headless
  * Description: Restrict access to the WordPress frontend for AMO needs
- * Version: 1.0.0
+ * Version: 2.0.0
  * License: MPL-2
  */
 
@@ -10,12 +10,18 @@ function addons_disable_frontend()
 {
     global $wp;
 
+    $isAuth0Callback =
+        $_SERVER['REQUEST_URI'] === '/index.php' &&
+        !empty($_COOKIE['auth0_state']);
+
     // We allow CRON tasks, REST API requests, requests to the admin interface,
-    // GraphQL requests, requests from an authenticated user. Everything else
-    // should be restricted.
-    $isRequestForbbiden = !defined('DOING_CRON') && !defined('REST_REQUEST') &&
-        !is_admin() && (empty($wp->query_vars['rest_oauth1']) &&
-        !defined('GRAPHQL_HTTP_REQUEST')) && !is_user_logged_in();
+    // requests to complete the Auth0 dance, and requests from an authenticated
+    // user.  Everything else should be restricted.
+    $isRequestForbbiden =
+        !defined('DOING_CRON') &&
+        !defined('REST_REQUEST') &&
+        !is_admin() & !is_user_logged_in() &&
+        !$isAuth0Callback;
 
     if ($isRequestForbbiden) {
         wp_die('Access is restricted, sorry.', 403);
